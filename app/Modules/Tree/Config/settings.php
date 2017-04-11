1fff4445feed5e6d4b609b0b3598e1d14cfa52f8
Alexander V <avodolazskii@weltkind.com>
2017-04-11 09:52:36 +0600

    fixed bug with

--- app/Modules/Tree/Config/settings.php
+++ app/Modules/Tree/Config/settings.php
@@ -7,6 +7,7 @@ return [
         "news"=>trans('news::admin.title'),
         "articles"=>trans('articles::admin.title'),
         "gallery"=>trans('gallery::admin.title'),
-        "feedback"=>trans('feedback::admin.title')],
+        "feedback"=>trans('feedback::admin.title'),
+        "branches" => trans('branches::admin.title')],
     "templates"=> ["inner"=>trans('tree::admin.templates.inner'), "index"=>trans('tree::admin.templates.index')]
 ];
--- app/Modules/Tree/Resources/Views/menu.blade.php
+++ app/Modules/Tree/Resources/Views/menu.blade.php
@@ -1,10 +1,5 @@
 @if (count($items))
 <ul class="nav navbar-nav">
-    <li>
-        @if ($page->parent_id)
-        <a href="{!! home() !!}">@lang('tree::index.home')</a>
-        @endif
-    </li>
     @foreach ($items as $item)
     <li {!!  (Request::is($item->slug) ? 'class="active"' : '')  !!}>
         <a href="{!! URL::route($item->slug) !!}">{{$item->title}}</a>
